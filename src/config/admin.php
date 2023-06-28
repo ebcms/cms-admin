ac8b6dd42be54e8d9721683975d548326505f454
John Doe <johndoe@example.com>
2023-06-28 11:06:51 +0800

    1

--- src/config/admin.php
+++ src/config/admin.php
@@ -1,29 +1,14 @@
 <?php
 
-use App\Ebcms\Admin\Model\Account;
 use App\Ebcms\CmsAdmin\Http\Category\Index;
 use App\Ebcms\CmsAdmin\Http\Content\Index as ContentIndex;
-use PsrPHP\Router\Router;
-use PsrPHP\Framework\Framework;
 
 return [
-    'menus' => Framework::execute(function (
-        Account $account,
-        Router $router
-    ): array {
-        $menus = [];
-        if ($account->checkAuth(Index::class)) {
-            $menus[] = [
-                'title' => '栏目管理',
-                'url' => $router->build('/ebcms/cms-admin/category/index'),
-            ];
-        }
-        if ($account->checkAuth(ContentIndex::class)) {
-            $menus[] = [
-                'title' => '内容管理',
-                'url' => $router->build('/ebcms/cms-admin/content/index'),
-            ];
-        }
-        return $menus;
-    })
+    'menus' => [[
+        'title' => '栏目管理',
+        'node' => Index::class,
+    ], [
+        'title' => '内容管理',
+        'node' => ContentIndex::class,
+    ]]
 ];
